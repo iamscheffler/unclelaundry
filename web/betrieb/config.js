0b317696bd9d25dcdc206723b4da9fa20ba128d5
iamscheffler <nikolaj.scheffler@gmail.com>
2026-09-22 22:28:50 +0200

    Add files via upload

--- web/betrieb/config.js
+++ web/betrieb/config.js
@@ -1,1 +1,9 @@
-
+/* ------------------------------------------------------------------
+   EINSTELLUNGEN für die Betriebs-Ansicht.
+   Dasselbe Supabase-Projekt wie in web/customer/config.js.
+   ------------------------------------------------------------------ */
+const CONFIG = {
+  name: "Uncle Laundry",
+  supabaseUrl: "https://tjxwwnpnsdhxvfgsgozx.supabase.co",
+  supabaseAnonKey: "sb_publishable_GoLQ3A6V9fud5SKo2KXnaA_DFTknNLg"
+};
